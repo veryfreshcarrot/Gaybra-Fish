bc58d515bbad9363490597eaf4ada447ed214ae8
veryfreshcarrot <sohminxuan@1utar.my>
2025-03-27 23:11:57 +0800

    Update install.sh to change repository clone destination from SegTracker to GayTracker

--- install.sh
+++ install.sh
@@ -22,7 +22,4 @@ uv pip install -r Gaybra/pyproject.toml --quiet
 uv pip install spatial-correlation-sampler --no-build-isolation
 
 # Clone the Segment-and-Track-Anything repository
-git clone https://github.com/z-x-yang/Segment-and-Track-Anything.git SegTracker
-
-# Note: The following line is commented out as it was in the original
-# uv pip install https://github.com/ClementPinard/Pytorch-Correlation-extension.git
+git clone https://github.com/z-x-yang/Segment-and-Track-Anything.git GayTracker
