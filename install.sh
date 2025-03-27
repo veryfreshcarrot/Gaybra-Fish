d29e972f83117521ea5ac169c71eeb64c5f597c9
veryfreshcarrot <sohminxuan@1utar.my>
2025-03-27 23:02:59 +0800

    z

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Change to the content directory
+cd /content
+
+# Install uv package manager
+pip install uv
+
+# Install dependencies from pyproject.toml
+uv pip install -r pyproject.toml --quiet
+
+# Install spatial-correlation-sampler without build isolation
+uv pip install spatial-correlation-sampler --no-build-isolation
+
+# Clone the Segment-and-Track-Anything repository
+git clone https://github.com/z-x-yang/Segment-and-Track-Anything.git SegTracker
+
+# Note: The following line is commented out as it was in the original
+# uv pip install https://github.com/ClementPinard/Pytorch-Correlation-extension.git
