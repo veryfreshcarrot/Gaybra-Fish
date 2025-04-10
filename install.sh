c01d28bea9934d06ac0d15b52af117c321c778f1
veryfreshcarrot <79198586+veryfreshcarrot@users.noreply.github.com>
2025-04-10 23:04:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,10 +16,10 @@ fi
 
 # Install dependencies from pyproject.toml
 echo "Installing dependencies from pyproject.toml..."
-uv pip install -r Gaybra/pyproject.toml
+uv run pip install -r Gaybra/pyproject.toml
 
 # Install spatial-correlation-sampler without build isolation
-uv pip install spatial-correlation-sampler --no-build-isolation
+uv run pip install spatial-correlation-sampler --no-build-isolation
 
 # Clone the Segment-and-Track-Anything repository
 git clone https://github.com/z-x-yang/Segment-and-Track-Anything.git GayTracker
